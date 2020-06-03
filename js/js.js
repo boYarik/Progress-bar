c43222fe6cf3aef5c3ac124a6f870f7426aa66e7
boYarik <65514377+boYarik@users.noreply.github.com>
2020-06-03 19:34:36 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,42 @@
+function dummy(){
+	console.log('Fucking js');
+};
+
+//let now = $('[aria-valuemin="0"]')
+//function plus1PerCent(){
+//	now = now + 1;
+//  console.log('now')
+//}
+
+
+
+//let now = $('[style.width(integer)');
+//now = Number.parseInt(val);
+//function plus1(){
+//	now = now + 1;
+//	console.log(now)
+//}
+	
+
+
+
+
+
+
+$(".plus1").click(dummy);
+$(".plus3").click(dummy);
+$(".plus7").click(dummy);
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('Не очкуй, код в норме')
